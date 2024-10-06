89f2dbce02237cffba52fc3606146ef9c7543806
Wilson-kang015 <wkang26_student@ivycollegiate.org>
2024-10-06 12:13:16 +0000

    Homework comments

--- Wilson_Sprint_Projectv2.75/Wilson_Sprint_Projectv2.76.py
+++ Wilson_Sprint_Projectv2.75/Wilson_Sprint_Projectv2.76.py
@@ -1,3 +1,5 @@
 # List all of the factors of a value
 def list_factors(n):
+    # Explain the line of code below
     return [i for i in range (1, n+1) if a % i == 0]
+    # Comment your explination here. This line of the code list out every number that n are divided by the number i, which is i.
